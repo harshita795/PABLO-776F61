72e2386655b9832a63a0d28115b16d97f36fdd23
harshita795 <yadavharshita261303@gmail.com>
2024-11-14 21:17:54 +0530

    feat: saveImages to db with validations

--- controllers/saveImages.js
+++ controllers/saveImages.js
@@ -4,6 +4,22 @@ const saveImages = async (req, res) => {
   try {
     const { imageUrl, description, altDescription, tags, userId } = req.body;
 
+    if (!imageUrl.startsWith("<https://images.unsplash.com/")) {
+      return res.status(400).json({ message: "Invalid image URL" });
+    }
+
+    if (tags.length > 5) {
+      return res.status(400).json({ message: "Cannot have more than 5 tags" });
+    }
+
+    for (let tag of tags) {
+      if (tag.length > 20) {
+        return res
+          .status(400)
+          .json({ message: "Each tag cannot exceed 20 characters." });
+      }
+    }
+
     const savedImage = await photo.create({
       imageUrl,
       description,
